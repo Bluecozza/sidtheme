aa98c4d85807bb522bb9f59fa10663bec0ff10bd
Pegasus <iblispega@yahoo.com>
2025-02-09 23:34:14 +0800

    style fix margin and padding

--- single.php
+++ single.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
 <div class="content-area">
-    <div class="container">
+    <div class="single-post-container">
         <?php
         while (have_posts()) : 
             the_post();
